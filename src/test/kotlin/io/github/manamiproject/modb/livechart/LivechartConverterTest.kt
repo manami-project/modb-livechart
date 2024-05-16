85fa6a1e7779a43f662c9dee7a54a9a3c0ffbd95
manami-project <13179375+manami-project@users.noreply.github.com>
2024-05-16 19:34:46 +0200

    Fixed test

--- src/test/kotlin/io/github/manamiproject/modb/livechart/LivechartConverterTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/livechart/LivechartConverterTest.kt
@@ -374,8 +374,8 @@ internal class LivechartConverterTest {
                 val result = converter.convert(testFile)
 
                 // then
-                assertThat(result.picture).isEqualTo(URI("https://github.com/manami-project/anime-offline-database/raw/master/pics/no_pic.png"))
-                assertThat(result.thumbnail).isEqualTo(URI("https://github.com/manami-project/anime-offline-database/raw/master/pics/no_pic_thumbnail.png"))
+                assertThat(result.picture).isEqualTo(URI("https://raw.githubusercontent.com/manami-project/anime-offline-database/master/pics/no_pic.png"))
+                assertThat(result.thumbnail).isEqualTo(URI("https://raw.githubusercontent.com/manami-project/anime-offline-database/master/pics/no_pic_thumbnail.png"))
             }
         }
 
